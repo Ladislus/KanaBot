0c4f88dc73c9600a8343977d97b342b94e6c10c8
Ladislus <walcak.ladislas@gmail.com>
2021-02-11 23:40:59 +0100

    [ADD] Added 'supportVarArgs' parameters to commandlist

--- bot/commands/commandlist.py
+++ bot/commands/commandlist.py
@@ -3,6 +3,7 @@ from .commandfunctions import test
 commandlist = {
     'test': {
         'argumentsRequired': 0,
+        'supportVarArgs': False,
         'optionalArgumentsSupport': [],
         'function': test
     }
