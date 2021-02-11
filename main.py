f82fdfb091203f356eded5942342e85fd960ba65
Ladislus <walcak.ladislas@gmail.com>
2021-02-12 00:34:46 +0100

    [ADD] Added main test in main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from config.globalconfig import globalConfig_from_file
 from bot.bot import Bot
 from env import TOKEN
 
-
-bot: Bot = Bot(globalConfig_from_file())
-bot.run(TOKEN)
+if __name__ == '__main__':
+    bot: Bot = Bot(globalConfig_from_file())
+    bot.run(TOKEN)
