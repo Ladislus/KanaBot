529c08803fc72044aedd4a7417a2da1a9185d92f
Ladislus <walcak.ladislas@gmail.com>
2021-02-11 23:37:46 +0100

    [MOD] Moved bot event from main to Bot class

---
+++ bot/bot.py
@@ -0,0 +1,30 @@
+from config.globalconfig import GlobalConfig
+from .commands.command import Command
+from discord import Client, Message
+from discord.utils import get
+from env import GUILD
+
+
+class Bot(Client):
+    def __init__(self, config: GlobalConfig, **options):
+        super().__init__(**options)
+        self._config: GlobalConfig = config
+
+    def __repr__(self):
+        return f'{self._config}'
+
+    async def on_ready(self, ):
+        # Problem while fetching the discord guild
+        guild = get(super().guilds, name=GUILD)
+        if guild is None:
+            exit()
+        # Else, save the guild informations
+        self._config._guild = guild
+
+        print(f'{super().user.name} is connected to the following guild: {guild.name}\n')
+        print(self._config)
+
+    async def on_message(self, msg: Message):
+        if Command.isValidCommand(msg, self._config):
+            com: Command = Command(msg)
+            print(com)
--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-from discord import Client, Message
-from discord.utils import get
-from command import Command
-from config import globalconfig
-from utils import isValidCommand
-import env
+from config.globalconfig import globalConfig_from_file
+from bot.bot import Bot
+from env import TOKEN
 
 
-client = Client()
-
-globalCfg = globalconfig.globalConfig_from_file()
-
-
-@client.event
-async def on_ready():
-    # Problem while fetching the discord guild
-    guild = get(client.guilds, name=env.GUILD)
-    if guild is None:
-        exit()
-    # Else, save the guild informations
-    globalCfg._guild = guild
-
-    print(f'{client.user.name} is connected to the following guild: {guild.name}\n')
-    print(globalCfg)
-
-
-@client.event
-async def on_message(msg: Message):
-    if isValidCommand(msg, globalCfg):
-        print('ok')
-        com: Command = Command(msg)
-        print(com)
-
-
-client.run(env.TOKEN)
+bot: Bot = Bot(globalConfig_from_file())
+bot.run(TOKEN)
