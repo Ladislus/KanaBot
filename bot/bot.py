ff8cb7a65ff90401078ffeb0a9828faf391e725f
Ladislus <walcak.ladislas@gmail.com>
2021-02-13 19:36:59 +0100

    [ADD] Added intent to get member list

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 from config.globalconfig import GlobalConfig
 from .commands.command import Command
-from discord import Client, Message
+from discord import Client, Message, Intents
 from discord.utils import get
 from .commands.commandparser import Parser
 from env import GUILD
@@ -8,15 +8,23 @@ from env import GUILD
 
 class Bot(Client):
     def __init__(self, config: GlobalConfig, **options):
-        super().__init__(**options)
+        # Option to enable list of members
+        intent: Intents = Intents.default()
+        intent.members = True
+        super().__init__(intents=intent, **options)
         self._config: GlobalConfig = config
 
     def __repr__(self):
         return f'{self._config}'
 
+    @property
+    def config(self):
+        return self._config
+
     async def on_ready(self, ):
         # Problem while fetching the discord guild
         guild = get(super().guilds, name=GUILD)
+        print(guild.members)
         if guild is None:
             exit()
         # Else, save the guild informations
@@ -27,5 +35,5 @@ class Bot(Client):
 
     async def on_message(self, msg: Message):
         if Command.isValidCommand(msg, self._config):
-            com: Command = Parser.parse(msg)
+            com: Command = Parser.parse(self, msg)
             await com.execute()
