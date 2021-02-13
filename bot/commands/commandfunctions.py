efbd47732c8ad1799f37c5c0b1fe195502e38011
Ladislus <walcak.ladislas@gmail.com>
2021-02-13 18:33:17 +0100

    [FIX] Fixed typo

--- bot/commands/commandfunctions.py
+++ bot/commands/commandfunctions.py
@@ -1,7 +1,7 @@
 from .commandelement import Element
 from discord import TextChannel
 
-async def test(args: dict, elements: dict[Element, object]):
 
+async def test(args: dict, elements: dict[Element, object]):
     chan: TextChannel = elements[Element.CHANNEL]
-    await chan.send("Niquez vos race avec les tests")
+    await chan.send("Niquez vos race**s** (Oui, je pense à toi enculai) avec les tests")
