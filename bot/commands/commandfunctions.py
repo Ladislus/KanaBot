f12ec3a7e8f710d2ac984c8b5166f38dfeb132ea
Ladislus <walcak.ladislas@gmail.com>
2021-02-13 20:09:23 +0100

    [MOD] Added None-check for mention

--- bot/commands/commandfunctions.py
+++ bot/commands/commandfunctions.py
@@ -3,17 +3,18 @@ from discord import TextChannel
 from discord.utils import get
 
 
-async def test(args: dict, elements: dict[Element, object]):
+async def test(args: dict, elements: dict):
     chan: TextChannel = elements[Element.CHANNEL]
     await chan.send("Niquez vos race**s** (Oui, je pense à toi enculai) avec les tests")
 
 
-async def enculai(args: dict, elements: dict[Element, object]):
+async def enculai(args: dict, elements: dict):
     chan: TextChannel = elements[Element.CHANNEL]
     mention: str = get(elements[Element.USERS], name='Dwindwin').mention
-    await chan.send(f'{mention}: tg encul**ai**')
+    if mention is not None:
+        await chan.send(f'{mention}: tg encul**ai**')
 
 
-async def ladislas(args: dict, elements: dict[Element, object]):
+async def ladislas(args: dict, elements: dict):
     chan: TextChannel = elements[Element.CHANNEL]
     await chan.send('Ladislus est un homme magnifique')
